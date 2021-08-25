13e50a160f54b86987b0d08156cc9d7ff204a142
Tsotne <cotne1989@gmail.com>
2021-08-25 15:55:19 +0400

    test

--- next-project/pages/api/verification/findPhone/index.js
+++ next-project/pages/api/verification/findPhone/index.js
@@ -21,7 +21,7 @@ export default async function handler(req, res){
                 select:{
                     smsConfirmed: true
                 },
-                where: {
+                where: { 
                     phone: phone
                 }
             });
