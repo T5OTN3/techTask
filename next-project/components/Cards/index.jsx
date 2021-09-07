815bb4e3c10c06b884a064d4351df2126cdf37a1
Tsotne <cotne1989@gmail.com>
2021-09-07 17:07:28 +0400

    add pagination real blogs card

--- next-project/components/Cards/index.jsx
+++ next-project/components/Cards/index.jsx
@@ -1,19 +1,51 @@
-import { Grid } from '@material-ui/core';
 import React from 'react';
-import MediaCard from './card'
+import BlogCard from './blogCard'
+import { useEffect, useState } from 'react'
+
+import ReactPaginate from 'react-paginate';
+
 
 const Cards = ({ data }) => {
+    const [blogs, setBlogs] = useState([]);
+    const [page, setPage] = useState(1);
+
+    const resPerPage = 6;
+
+    useEffect(() => {
+        const start = (page - 1) * resPerPage;
+        const end = page * resPerPage;
+        setBlogs(data.slice(start, end));
+    })
+
+    const selectPage = ({ selected }) => {
+            setPage(selected + 1);
+            const start = (page - 1) * resPerPage;
+            const end = page * resPerPage;
+            setBlogs(data.slice(start, end));
+    }
+
 
     return (
-        <Grid container spacing={2}>
-            {
-                data.map((el, index) => (
-                    <Grid key={index} item md={4} sm={6} xs={12}>
-                        <MediaCard id={el.id} author={el.author} content={el.content}/>
-                    </Grid>
-                ))
-            }
-        </Grid>
+        <>
+            <section class="flex flex-row flex-wrap mx-auto">
+                {
+                    blogs.map((el, index) => (
+                            <BlogCard id={el.id} author={el.author} content={el.content}/> 
+                    ))
+                }
+            </section>
+            <ReactPaginate 
+                pageCount={data.length / resPerPage}
+                containerClassName='bg-white px-4 py-3 flex items-center justify-between border-t border-gray-200 sm:px-6'
+                pageClassName='relative z-0 inline-flex rounded-md shadow-sm -space-x-px'
+                pageLinkClassName='bg-white border-gray-300 text-gray-500 hover:bg-gray-50 relative inline-flex items-center px-4 py-2 border text-sm font-medium'
+                activeClassName='z-10 bg-indigo-50 border-indigo-500 text-indigo-600 relative inline-flex items-center px-1 py-1 border text-sm font-medium'
+                previousLabel={<div>Preview</div>}
+                nextLabel={<div>Next</div>}
+                onPageChange={selectPage}
+            />
+        </>
+        
     )
 }
 
