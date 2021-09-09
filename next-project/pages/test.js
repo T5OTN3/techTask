13676613c48146a746aadd800c6cb793d6b3ccfe
Tsotne <cotne1989@gmail.com>
2021-09-10 01:54:56 +0400

    add image upload and preview

--- next-project/pages/test.js
+++ next-project/pages/test.js
@@ -1,173 +1,84 @@
-import Head from 'next/head';
-import Content from './../widgets/content';
 import { useEffect, useState } from 'react'
-
-import ReactPaginate from 'react-paginate';
-
-function Example({personArr}) {
-  return (
-    <ul className="divide-y divide-gray-200">
-      {personArr.map((person) => (
-        <li key={person.email} className="py-4 flex">
-          <img className="h-10 w-10 rounded-full" src={person.image} alt="" />
-          <div className="ml-3">
-            <p className="text-sm font-medium text-gray-900">{person.name}</p>
-            <p className="text-sm text-gray-500">{person.email}</p>
-          </div>
-        </li>
-      ))}
-    </ul>
-  )
-}
+import { CloudUploadIcon } from '@heroicons/react/solid'
+import Alert from '@material-ui/lab/Alert';
+import Collapse from '@material-ui/core/Collapse';
 
 
-export default function Home({ blogs }) {
-    const [persons, setPersons] = useState([]);
-    const [page, setPage] = useState(1);
 
-    const resPerPage = 6;
 
-    useEffect(() => {
-        const start = (page - 1) * resPerPage;
-        const end = page * resPerPage;
-        setPersons(people.slice(start, end));
-    })
+export default function Home() {
+  const [openErrorAlert, setOpenErrorAlert] = useState(false);
+  const [images, setImages] = useState([]);
+  const [preview, setPreview] = useState([])
 
-  const selectPage = ({ selected }) => {
-        setPage(selected + 1);
-        const start = (page - 1) * resPerPage;
-        const end = page * resPerPage;
-        setPersons(people.slice(start, end));
+  // create a preview as a side effect, whenever selected file is changed
+  useEffect(() => {
+      let arrUrl = [];
+
+      if (!images) {
+          setPreview(undefined)
+          return
+      }
+
+      images && [...images].map((file) => {
+        arrUrl.push(URL.createObjectURL(file));
+      })
+      setPreview(arrUrl);
+
+      // free memory when ever this component is unmounted
+      return () => URL.revokeObjectURL(arrUrl)
+  }, [images])
+
+  const _onChange = (e) => {
+    if(e.target.files.length > 10){
+      setOpenErrorAlert(true)
+      return
+    }
+    setImages(e.target.files);
+  }
+
+  const _removeImage = (index) => {
+    setImages([...images].filter((file, i) => i != index));
   }
 
   return (
     <div>
-      <Head>
-        <title>Main</title>
-      </Head>
-        <Example personArr={persons}/>
-          <ReactPaginate 
-            pageCount={18 / resPerPage}
-            containerClassName='bg-white px-4 py-3 flex items-center justify-between border-t border-gray-200 sm:px-6'
-            pageClassName='relative z-0 inline-flex rounded-md shadow-sm -space-x-px'
-            pageLinkClassName='bg-white border-gray-300 text-gray-500 hover:bg-gray-50 relative inline-flex items-center px-4 py-2 border text-sm font-medium'
-            activeClassName='z-10 bg-indigo-50 border-indigo-500 text-indigo-600 relative inline-flex items-center px-1 py-1 border text-sm font-medium'
-            previousLabel={<div>Preview</div>}
-            nextLabel={<div>Next</div>}
-            onPageChange={selectPage}
-          />
-      <Content/>
+
+      <Collapse in={openErrorAlert} timeout='auto'>
+        <Alert severity="error" onClose={() => {setOpenErrorAlert(false);}}>You can select maximum 10 image</Alert>
+      </Collapse>
+      <form>
+      <div className="bg-grey-lighter">
+        <label className="w-40 flex flex-col items-center px-2 py-3 text-blue rounded-lg shadow-lg tracking-wide border border-blue cursor-pointer">
+          <CloudUploadIcon className="w-8 h-8"/>
+          <span className="mt-2 text-base leading-normal">Images</span>
+          <input
+          className="hidden"
+          type="file"
+          name="blogImages"
+          multiple={true}
+          accept="image/*"
+          onChange={(e) => _onChange(e) }
+        />
+        </label>
+      </div>
+      </form>
+      <div className="flex flex-wrap py-3 border-dotted border-4 border-light-blue-500">
+        {preview && preview.map((file, key) => {
+          return (   
+              <div key={key} className="px-2 py-3 relative">
+                  <button
+                    className="absolute bg-transparent text-2xl text-red-500 font-semibold leading-none right-1 top-0 outline-none focus:outline-none"
+                    onClick={() => _removeImage(key)}
+                  >
+                    <span>×</span>
+                  </button>
+                  <img className="object-contain h-20 w-full" src={file}  alt={file.name}/>
+              </div>   
+          )
+        })}
+      </div>
     </div>
   )
 }
 
-const people = [
-    {
-      name: 'Calvin Hawkins 1',
-      email: 'calvin.hawkins@example.com',
-      image:
-        'https://images.unsplash.com/photo-1491528323818-fdd1faba62cc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
-    },
-    {
-      name: 'Kristen Ramos 2',
-      email: 'kristen.ramos@example.com',
-      image:
-        'https://images.unsplash.com/photo-1550525811-e5869dd03032?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
-    },
-    {
-      name: 'Ted Fox 3',
-      email: 'ted.fox@example.com',
-      image:
-        'https://images.unsplash.com/photo-1500648767791-00dcc994a43e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
-    },
-    {
-        name: 'Calvin Hawkins 4',
-        email: 'calvin.hawkins@example.com',
-        image:
-          'https://images.unsplash.com/photo-1491528323818-fdd1faba62cc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
-      },
-      {
-        name: 'Kristen Ramos 5',
-        email: 'kristen.ramos@example.com',
-        image:
-          'https://images.unsplash.com/photo-1550525811-e5869dd03032?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
-      },
-      {
-        name: 'Ted Fox 6',
-        email: 'ted.fox@example.com',
-        image:
-          'https://images.unsplash.com/photo-1500648767791-00dcc994a43e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
-      },
-      {
-        name: 'Calvin Hawkins 7',
-        email: 'calvin.hawkins@example.com',
-        image:
-          'https://images.unsplash.com/photo-1491528323818-fdd1faba62cc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
-      },
-      {
-        name: 'Kristen Ramos 8',
-        email: 'kristen.ramos@example.com',
-        image:
-          'https://images.unsplash.com/photo-1550525811-e5869dd03032?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
-      },
-      {
-        name: 'Ted Fox 9',
-        email: 'ted.fox@example.com',
-        image:
-          'https://images.unsplash.com/photo-1500648767791-00dcc994a43e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
-      },
-      {
-        name: 'Calvin Hawkins 10',
-        email: 'calvin.hawkins@example.com',
-        image:
-          'https://images.unsplash.com/photo-1491528323818-fdd1faba62cc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
-      },
-      {
-        name: 'Kristen Ramos 11',
-        email: 'kristen.ramos@example.com',
-        image:
-          'https://images.unsplash.com/photo-1550525811-e5869dd03032?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
-      },
-      {
-        name: 'Ted Fox 12',
-        email: 'ted.fox@example.com',
-        image:
-          'https://images.unsplash.com/photo-1500648767791-00dcc994a43e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
-      },
-      {
-        name: 'Calvin Hawkins 13',
-        email: 'calvin.hawkins@example.com',
-        image:
-          'https://images.unsplash.com/photo-1491528323818-fdd1faba62cc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
-      },
-      {
-        name: 'Kristen Ramos 14',
-        email: 'kristen.ramos@example.com',
-        image:
-          'https://images.unsplash.com/photo-1550525811-e5869dd03032?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
-      },
-      {
-        name: 'Ted Fox 15',
-        email: 'ted.fox@example.com',
-        image:
-          'https://images.unsplash.com/photo-1500648767791-00dcc994a43e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
-      },
-      {
-        name: 'Calvin Hawkins 16',
-        email: 'calvin.hawkins@example.com',
-        image:
-          'https://images.unsplash.com/photo-1491528323818-fdd1faba62cc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
-      },
-      {
-        name: 'Kristen Ramos 17',
-        email: 'kristen.ramos@example.com',
-        image:
-          'https://images.unsplash.com/photo-1550525811-e5869dd03032?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
-      },
-      {
-        name: 'Ted Fox 18',
-        email: 'ted.fox@example.com',
-        image:
-          'https://images.unsplash.com/photo-1500648767791-00dcc994a43e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
-      },
-  ]
