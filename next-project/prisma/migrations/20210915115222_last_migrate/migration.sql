258495b64a6d49374c4d68d766bbfb2c646978d3
Tsotne <cotne1989@gmail.com>
2021-09-15 16:42:32 +0400

    delete migrate file

--- next-project/prisma/migrations/20210915115222_last_migrate/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to alter the column `image360` on the `Images` table. The data in that column could be lost. The data in that column will be cast from `VarChar(255)` to `TinyInt`.
-
-*/
--- AlterTable
-ALTER TABLE `Images` MODIFY `image360` BOOLEAN NOT NULL DEFAULT false;
